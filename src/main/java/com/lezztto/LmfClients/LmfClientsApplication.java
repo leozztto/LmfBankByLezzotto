2b1dafcb7b3a40bcfb13e1211a424a117186d0ca
LeandroMF <leozztto@gmail.com>
2026-04-28 19:02:14 -0300

    Commit base

---
+++ src/main/java/com/lezztto/LmfClients/LmfClientsApplication.java
@@ -0,0 +1,13 @@
+package com.lezztto.LmfClients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmfClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmfClientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lezztto/LmfClients/LmfClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lezztto.LmfClients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmfClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
