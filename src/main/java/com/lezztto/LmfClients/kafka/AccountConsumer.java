f52b239e79b3cfd541af59f463aea2fd765535f5
LeandroMF <leozztto@gmail.com>
2026-04-30 14:51:21 -0300

    gerado configuracoes e classes para ler menasgens do topico kafka

---
+++ src/main/java/com/lezztto/LmfClients/kafka/AccountConsumer.java
@@ -0,0 +1,53 @@
+package com.lezztto.LmfClients.kafka;
+
+import com.lezztto.LmfClients.kafka.event.domain.dto.ProcessedEventDto;
+import com.lezztto.LmfClients.kafka.dto.AccountEvent;
+import com.lezztto.LmfClients.kafka.mapper.AccountEventMapper;
+import com.lezztto.LmfClients.kafka.event.service.ProcessedEventService;
+import com.lezztto.LmfClients.account.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class AccountConsumer {
+
+    private final AccountService accountService;
+
+    private final AccountEventMapper accountEventMapper;
+
+    private final ProcessedEventService processedEventService;
+
+    @KafkaListener(
+            topics = "account-create",
+            containerFactory = "kafkaListenerContainerFactory"
+    )
+    public void consume(AccountEvent accountEvent, Acknowledgment ack) {
+
+        log.info("Received event: {}", accountEvent.getEventId());
+
+        try {
+            var processedEventDto = new ProcessedEventDto(accountEvent.getEventId(), LocalDateTime.now());
+            if (!processedEventService.tryProcess(processedEventDto)) {
+                return;
+            }
+
+            var account = accountEventMapper.eventToDto(accountEvent);
+            accountService.create(account);
+
+            processedEventService.markAsProcessed(processedEventDto);
+
+            ack.acknowledge();
+
+        } catch (Exception e) {
+            log.error("Error processing event: {}", accountEvent, e);
+            throw e;
+        }
+    }
+}
